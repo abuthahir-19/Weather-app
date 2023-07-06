7fe5458cc2b5325c3059506af6c971c0959f6b07
abuthahir-19 <abuthahirstar1@gmail.com>
2023-07-06 10:51:05 +0530

    updated changes

--- src/components/RightPane.jsx
+++ src/components/RightPane.jsx
@@ -5,7 +5,7 @@ import Profile from '../images/IMG_5926.jpg';
 const RightPane = () => {
     return (
         <div className=" bg-sky-100 w-[75%] absolute right-0 top-0">
-            <div className="flex justify-between">
+            <div className="flex justify-between p-6">
                 <div className="p-2">
                     <h2 className="font-medium text-2xl">Welcome back Isabella !</h2>
                     <p className="font-normal text-lg">Check out today's weather information </p>
@@ -14,10 +14,8 @@ const RightPane = () => {
                     <img className="h-5 w-5 cursor-pointer" src={ optionsIcon } alt="optionsIcon" />
                     <img className="h-10 w-10 object-cover cursor-pointer rounded-lg" src={ Profile } alt="profileImg" />
                 </div>
-                <div>
-                <h3 className="font-medium text-2xl">More details of today's weather</h3>
-                </div>
             </div>
+            <h3 className="font-medium text-2xl p-6">More details of today's weather</h3>
         </div>
     )
 };
